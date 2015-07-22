c16ccd60737835ea716f7a26b32218ba646555ad
GuillermoParedes <gdavid.ptorrez@gmail.com>
2015-07-22 15:18:39 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Guillermo David on 22/07/2015.
+ */
+module.exports = function(grunt){
+
+    //load all grunt tasks XD
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+
+        copy : {
+            main : {
+                expand : true,
+                cwd : 'src/',
+                src : ["**", "!css/**/*.less","!css/**/*.scss"],
+                dest : 'dist/'
+            }
+        },
+        less : {
+            options : {
+                paths : ['src/css']
+            },
+            src : {
+                expand : true,
+                cwd : 'src/css',
+                src : "*.less",
+                ext : ".css",
+                dest : 'src/css'
+            }
+        },
+        sass : {
+            files : [{
+                expand : true,
+                cwd : "src/css",
+                src : "*.scss",
+                ext : ".css",
+                dest : "src/css"
+            }]
+        },
+        watch : {
+            options : {
+                nospawn : true,
+                livereload : true
+            },
+            less : {
+                files : ['src/css/**/*.less'],
+                tasks : ['less']
+            },
+            sass : {
+                files : ['src/css/**/*.scss'],
+                tasks : ['sass']
+            },
+            copy : {
+                files : ['src/**'],
+                tasks : ['copy:main']
+            }
+        }
+
+    });
+    grunt.registerTask('default', ['watch']);
+};
